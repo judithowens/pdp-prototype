c66000712e946bf5e46835609da07241eeddd76d
Judith Owens <judith.owens@civica.co.uk>
2019-10-18 18:00:51 +0100

    Webpacker and Bootstrap now playing nicely

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/main'
+import 'bootstrap/dist/js/bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+})
