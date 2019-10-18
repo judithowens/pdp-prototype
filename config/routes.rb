5be468bd22b0308d798fbe066e95f3bb3fca224c
Judith Owens <judith.owens@civica.co.uk>
2019-10-18 14:55:01 +0100

    Loading up Pundit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'forgot_password', to: 'devise/passwords#new'
+    get 'reset_password', to: 'devise/passwords#edit'
+  end
+
   resources :users
   resources :roles
 
